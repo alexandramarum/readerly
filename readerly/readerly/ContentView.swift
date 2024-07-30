4399f022d66ca3948675994c109eeeeebb269dbb
alexandramarum <alexandramarum@gmail.com>
2024-07-30 15:16:03 -0400

    Inital commit

---
+++ readerly/readerly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  readerly
+//
+//  Created by Alexandra Marum on 7/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ readerly/readerly/readerlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  readerlyApp.swift
+//  readerly
+//
+//  Created by Alexandra Marum on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct readerlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
